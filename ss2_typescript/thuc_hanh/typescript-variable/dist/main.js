0d64f8488086f8e7faf65d36ed2feeeac48630f1
trangiang911 <tbgiang191199@gmail.com>
2022-06-05 21:48:36 +0700

    up bai

---
+++ ss2_typescript/thuc_hanh/typescript-variable/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let wight;
+let height;
+wight = 10.5;
+height = 20;
+let area = wight * height;
+console.log(`dien tich la: ${area}`);
---
+++ ss2_typescript/thuc_hanh/typescript-variable/main.ts
@@ -0,0 +1,6 @@
+let wight:number;
+let height:number;
+wight=10.5;
+height=20;
+let area:number=wight*height;
+console.log(`dien tich la: ${area}`);
