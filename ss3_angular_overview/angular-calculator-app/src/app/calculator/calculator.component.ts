d14688fda1a2d125e43b27ab18f4bea67d7bef64
trangiang911 <tbgiang191199@gmail.com>
2022-06-06 09:08:13 +0700

    up bai

---
+++ ss3_angular_overview/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  so(value: string) {
+    let result=(document.getElementById('result') as HTMLInputElement).value;
+    (document.getElementById('result') as HTMLInputElement).value=result+value
+  }
+
+ 
